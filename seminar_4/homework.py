a95a6952271fbb899931292a4d1d34946e169747
Alena Savostyanova <el.savostyanova90@yandex.ru>
2024-01-20 10:21:35 +0300

    Hw 4.

--- seminar_4/homework.py
+++ seminar_4/homework.py
@@ -8,18 +8,18 @@
 from statistics import mean
 
 
-def correlation(number_list_x: list, number_list_y: list) -> float:
+def correlation_coefficient(number_list_x: list, number_list_y: list) -> float:
     """Pearson correlation coefficient."""
-    mean_lst1 = mean(number_list_x)
-    mean_lst2 = mean(number_list_y)
+    mean_list_x = mean(number_list_x)
+    mean_list_y = mean(number_list_y)
     return sum(
-        map(lambda x, y: (x - mean_lst1) * (y - mean_lst2),
+        map(lambda x, y: (x - mean_list_x) * (y - mean_list_y),
             number_list_x, number_list_y)) / (
         sum(
-            map(lambda x: (x - mean_lst1) ** 2, number_list_x)) * sum(
-            map(lambda y: (y - mean_lst2) ** 2, number_list_y))) ** 0.5
+            map(lambda x: (x - mean_list_x) ** 2, number_list_x)) * sum(
+            map(lambda y: (y - mean_list_y) ** 2, number_list_y))) ** 0.5
 
 
 list_x = [1, 2, 3, 4, 5]
 list_y = [2, 3, 6, 8, 10]
-print(correlation(list_x, list_y))
+print(correlation_coefficient(list_x, list_y))
