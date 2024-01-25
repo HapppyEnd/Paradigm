007df108224de466fafc544f7ead56dd17443ee6
Alena Savostyanova <el.savostyanova90@yandex.ru>
2024-01-25 13:12:47 +0300

    Hw 5.

---
+++ seminar_5/homework.pl
@@ -0,0 +1,9 @@
+sum_list([], 0).
+
+sum_list([Head|Tail], Sum) :-
+    sum_list(Tail, TailSum),
+    Sum is Head + TailSum.
+
+?- sum_list([1, 2, 3, 4, 5, 6], Sum).
+
+?- sum_list([10, 20, 30], Sum).
